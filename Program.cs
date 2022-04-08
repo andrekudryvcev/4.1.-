8fad8057d6f108c8096c494a395bf100ad5c48ea
Андрей Кудрявцев <andrekudryvcev@yahoo.com>
2022-04-08 22:26:06 +0500

    Добавлен болок switch

--- Program.cs
+++ Program.cs
@@ -96,6 +96,47 @@ namespace MyNamespace
                 Console.WriteLine("Your color is cyan!");
             }
 
+            Console.WriteLine("\n***************************************************\n");
+
+
+
+
+            Console.WriteLine("switch (переключатель)\n");
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color1 = Console.ReadLine();
+
+            switch (color1)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow");
+                    break;
+            }
+
+
+
 
         }
     }
